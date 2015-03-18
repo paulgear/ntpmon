f62b27c876225c0cb28695bc9a72bafe01ceb1e7
Paul Gear <github@libertysys.com.au>
2015-03-19 09:25:06 +1000

    Implement the core logic of the parser from ntpmon; add more tests; learn the difference between class & instance objects

--- check_ntpmon.py
+++ check_ntpmon.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 #
 # Author:       Paul Gear
-# Copyright:	(c) 2015 Gear Consulting Pty Ltd <http://libertysys.com.au/>
-# License:	GPLv3 <http://www.gnu.org/licenses/gpl.html>
-# Description:  NTP metrics as defined by ntpmon as a Nagios check.
+# Copyright:    (c) 2015 Gear Consulting Pty Ltd <http://libertysys.com.au/>
+# License:      GPLv3 <http://www.gnu.org/licenses/gpl.html>
+# Description:  NTP metrics as a Nagios check.
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -20,30 +20,24 @@
 #
 
 import re
+import warnings
 
 
 def ishostnamey(name):
-    """Return true if the passed name is roughly hostnamey.  NTP is casual about how it reports
-    hostnames and IP addresses, so we can't be too strict.  This function simply tests that all of
-    the characters in the string are letters, digits, dash, or period."""
+    """Return true if the passed name is roughly hostnamey.  NTP is rather casual about how it
+    reports hostnames and IP addresses, so we can't be too strict.  This function simply tests
+    that all of the characters in the string are letters, digits, dash, or period."""
     return re.search(r'^[\w.-]*$', name) is not None and name.find('_') == -1
 
 
 def isipaddressy(name):
-    """Return true if the passed name is roughly ip addressy.  NTP is very 'casual' about how it
+    """Return true if the passed name is roughly IP addressy.  NTP is rather casual about how it
     reports hostnames and IP addresses, so we can't be too strict.  This function simply tests
     that all of the characters in the string are hexadecimal digits, period, or colon."""
     return re.search(r'^[0-9a-f.:]*$', name) is not None
 
 
 class CheckNTPMon(object):
-
-    okpeers = 0
-    warnpeers = 0
-    critoffset = 0
-    warnoffset = 0
-    critreach = 0
-    warnreach = 0
 
     def __init__(self, warnpeers=2, okpeers=4, warnoffset=10, critoffset=50, warnreach=75,
             critreach=50):
@@ -105,17 +99,113 @@ class CheckNTPMon(object):
 
     def sync(self, synchost):
         """Return true if the synchost is non-zero in length and is a roughly valid host identifier"""
-        return len(synchost) > 0 and (ishostnamey(synchost) or isipaddressy(synchost))
+        synced = len(synchost) > 0 and (ishostnamey(synchost) or isipaddressy(synchost))
+        if synced:
+            print "OK: time is in sync with %s" % (synchost)
+        else:
+            print "CRITICAL: no sync host selected"
+        return synced
 
 
-#class NTPData(object):
-#
-#    def __init__(self, peerlines):
-#        pass
-#
+class NTPPeers(object):
+    """Turn the peer lines returned by 'ntpq -pn' into a data structure usable for checks."""
+
+    def __init__(self, peerlines):
+        self.ntpdata = {
+                'survivors': 0,
+                'offsetsurvivors': 0,
+                'discards': 0,
+                'offsetdiscards': 0,
+                'unknown': 0,
+                'peers': 0,
+                'offsetall': 0,
+                'totalreach': 0,
+                }
+
+        for l in peerlines:
+            if re.search(r'remote\s+refid\s+st\s+t\s+when\s+poll\s+reach\s+', l) is not None:
+                continue
+            if re.search(r'^=*$', l) is not None:
+                # this matches blank line as well the header
+                continue
+            if re.search(r'No association ID.s returned', l) is not None:
+                continue
+
+            # first column is the tally field, the rest are whitespace-separated fields
+            tally = l[0]
+            fields = l[1:-1].split()
+            if len(fields) != 10:
+                warnings.warn('Invalid ntpq peer line - there are %d fields: %s' % (len(fields), l))
+                continue
+
+            fieldnames = ['peer', 'refid', 'stratum', 'type', 'lastpoll', 'interval', 'reach',
+                    'delay', 'offset', 'jitter']
+            peerdata = dict(zip(fieldnames, fields))
+
+            if peerdata['peer'] in [".LOCL.", ".INIT.", ".XFAC."]:
+                continue
+
+            # see the explanation of tally codes in the ntpq documentation for how these work:
+            # - http://www.eecis.udel.edu/~mills/ntp/html/decode.html#peer
+            # - http://www.eecis.udel.edu/~mills/ntp/html/ntpq.html
+            # - http://psp2.ntp.org/bin/view/Support/TroubleshootingNTP
+
+            if tally in ['*', 'o'] and 'syncpeer' not in self.ntpdata:
+                # this is our sync peer
+                self.ntpdata['syncpeer'] = peerdata['peer']
+                self.ntpdata['offsetsyncpeer'] = abs(float(peerdata['offset']))
+                self.ntpdata['survivors'] += 1
+                self.ntpdata['offsetsurvivors'] += abs(float(peerdata['offset']))
+            elif tally in ['+', '#']:
+                # valid peer
+                self.ntpdata['survivors'] += 1
+                self.ntpdata['offsetsurvivors'] += abs(float(peerdata['offset']))
+            elif tally in [' ', 'x', '.', '-']:
+                # discarded peer
+                self.ntpdata['discards'] += 1
+                self.ntpdata['offsetdiscards'] += abs(float(peerdata['offset']))
+            else:
+                self.ntpdata['unknown'] += 1
+                warnings.warn('Unknown tally code detected - please report a bug: %s' % (l))
+                continue
+
+            self.ntpdata['peers'] += 1
+            self.ntpdata['offsetall'] += abs(float(peerdata['offset']))
+
+            # reachability - this counts the number of bits set in the reachability field
+            # (which is displayed in octal in the ntpq output)
+            # http://stackoverflow.com/questions/9829578/fast-way-of-counting-bits-in-python
+            self.ntpdata['totalreach'] += bin(int(peerdata['reach'], 8)).count("1")
+
+        # reachability as a percentage of the last 8 polls, across all peers
+        self.ntpdata['reachability'] = float(self.ntpdata['totalreach']) * 100 / self.ntpdata['peers'] / 8
+
+        # average offsets
+        if self.ntpdata['survivors'] > 0:
+            self.ntpdata['averageoffsetsurvivors'] = \
+                    self.ntpdata['offsetsurvivors'] / self.ntpdata['survivors']
+        if self.ntpdata['discards'] > 0:
+            self.ntpdata['averageoffsetdiscards'] = \
+                    self.ntpdata['offsetdiscards'] / self.ntpdata['discards']
+        self.ntpdata['averageoffset'] = self.ntpdata['offsetall'] / self.ntpdata['peers']
+
+    def dump(self):
+        if self.ntpdata['syncpeer']:
+            print "Synced to: %s, offset %g" % (self.ntpdata['syncpeer'],
+                    self.ntpdata['offsetsyncpeer'])
+        print "%d peers, average offset %g" % (self.ntpdata['peers'],
+                self.ntpdata['averageoffset'])
+        if self.ntpdata['survivors'] > 0:
+            print "%d good peers, average offset %g" % (self.ntpdata['survivors'],
+                    self.ntpdata['averageoffsetsurvivors'])
+        if self.ntpdata['discards'] > 0:
+            print "%d discarded peers, average offset %g" % (self.ntpdata['discards'],
+                    self.ntpdata['averageoffsetdiscards'])
+        print "Average reachability of all peers: %d%%" % (self.ntpdata['reachability'])
+
 
 def main():
-    # parse args
+    # parse args - if none, show current data & interpretation, plus how to use as Nagios check
     # call ntpq -pn
     # parse ntpq output
     # check results
@@ -125,4 +215,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- test_check_ntpmon.py
+++ test_check_ntpmon.py
@@ -20,7 +20,33 @@
 #
 
 import unittest
-from check_ntpmon import CheckNTPMon
+from check_ntpmon import CheckNTPMon, NTPPeers
+
+testdata = ["""
+     remote           refid      st t when poll reach   delay   offset  jitter
+==============================================================================
+ 137.189.4.10    .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+ 128.199.253.156 .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+ 103.233.241.1   .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+ 128.199.169.185 .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+*91.189.89.199   131.188.3.220    2 u 1019 1024  377  346.845   -0.598   1.105
+ 192.189.54.33   .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+ 129.250.35.250  .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+ 27.54.95.11     .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+ 54.252.129.186  .STEP.          16 u    - 1024    0    0.000    0.000   0.000
+""",
+
+"""     remote           refid      st t when poll reach   delay   offset  jitter
+==============================================================================
+-203.19.252.1    210.9.192.50     2 u  456 1024  377   47.629   -1.526  35.084
+*202.60.94.11    223.252.32.9     2 u  618 1024  377   18.785    0.259   0.371
++202.60.94.15    223.252.32.9     2 u  635 1024  377   19.333    0.038   0.851
++54.252.129.186  223.252.32.9     2 u  927 1024  377   36.072    0.337   1.043
+-192.168.1.2     203.23.237.200   3 u  788 1024  377    0.627   -0.605   0.162
+-192.168.1.1     203.23.237.200   3 u  123 1024  377    0.299   -0.941   0.329
+ 192.168.1.21    .INIT.          16 u    - 1024    0    0.000    0.000   0.000
+""",
+]
 
 
 class TestCheckNTPMon(unittest.TestCase):
@@ -69,11 +95,36 @@ class TestCheckNTPMon(unittest.TestCase):
         check = CheckNTPMon()
 
         self.assertFalse(check.sync(''))
+        self.assertFalse(check.sync('    '))
+        self.assertFalse(check.sync('!@#$%^&*()'))
         self.assertTrue(check.sync('blah.example.com'))
         self.assertTrue(check.sync('192.168.2.1'))
         self.assertTrue(check.sync('fe80::1'))
         self.assertTrue(check.sync('ds002.dedicated'))
         self.assertTrue(check.sync('node01.au.serve'))
+
+    def test_NTPPeers(self):
+        ntp = NTPPeers(testdata[0].split("\n"))
+        self.assertEqual(ntp.ntpdata['syncpeer'], '91.189.89.199')
+        self.assertEqual(ntp.ntpdata['offsetsyncpeer'], 0.598)
+        self.assertEqual(ntp.ntpdata['survivors'], 1)
+        self.assertEqual(ntp.ntpdata['averageoffsetsurvivors'], 0.598)
+        self.assertEqual(ntp.ntpdata['discards'], 8)
+        self.assertEqual(ntp.ntpdata['averageoffsetdiscards'], 0)
+        self.assertEqual(ntp.ntpdata['peers'], 9)
+        self.assertEqual(ntp.ntpdata['averageoffset'], 0.06644444444444444)
+        self.assertEqual(ntp.ntpdata['reachability'], 11.11111111111111)
+
+        ntp = NTPPeers(testdata[1].split("\n"))
+        self.assertEqual(ntp.ntpdata['syncpeer'], '202.60.94.11')
+        self.assertEqual(ntp.ntpdata['offsetsyncpeer'], 0.259)
+        self.assertEqual(ntp.ntpdata['survivors'], 3)
+        self.assertEqual(ntp.ntpdata['averageoffsetsurvivors'], 0.21133333333333335)
+        self.assertEqual(ntp.ntpdata['discards'], 4)
+        self.assertEqual(ntp.ntpdata['averageoffsetdiscards'], 0.768)
+        self.assertEqual(ntp.ntpdata['peers'], 7)
+        self.assertEqual(ntp.ntpdata['averageoffset'], 0.5294285714285715)
+        self.assertEqual(ntp.ntpdata['reachability'], 85.71428571428571)
 
 
 if __name__ == "__main__":
