67ee565b75694de0607b7c7ec10b0ac7dfb06388
Paul Gear <github@libertysys.com.au>
2017-03-13 19:27:59 +1000

    First cut at juju charms reactive layer

---
+++ reactive/ntpmon.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017 Canonical Ltd
+
+from charmhelpers.fetch import apt_install
+from charmhelpers.core import hookenv, host
+
+from charms.reactive import (
+    main,
+    remove_state,
+    set_state,
+)
+
+from charms.reactive.decorators import (
+    hook,
+    when,
+    when_file_changed,
+    when_not,
+)
+
+import os
+import subprocess
+
+
+#
+# Module configuration - these shouldn't need adjusting
+#
+
+ntp_conf = '/etc/ntp.conf'
+ntpmon_dir = '/opt/ntpmon/'
+service_name = 'ntpmon-telegraf'
+systemd_config = '/etc/systemd/system/' + service_name + '.service'
+upstart_config = '/etc/init/' + service_name + '.conf'
+
+#
+# Hooks
+#
+
+
+# layer-basic does not fire config.changed unconditionally on charm
+# upgrade, which means we need to make that happen ourselves, in
+# case there is a configuration-affecting change.
+
+@hook('upgrade-charm')
+def upgrade_charm():
+    hookenv.log('Forcing NTPmon upgrade on upgrade-charm')
+    remove_state('ntpmon.installed')
+
+
+#
+# Reactive state handlers
+#
+
+
+@when_not('ntpmon.installed')
+def install_ntpmon():
+    """
+    Install package dependencies, source files, and startup configuration.
+    """
+    hookenv.log('installing ntpmon dependencies')
+    apt_install(['python3-psutil'])
+
+    hookenv.log('installing ntpmon')
+    host.rsync('src/', ntpmon_dir)
+
+    if host.init_is_systemd():
+        hookenv.log('installing ntpmon systemd configuration')
+        host.rsync('src/' + service_name + '.systemd', systemd_config)
+        subprocess.call(['systemd', 'daemon-reload'])
+        host.service('enable', service_name)
+    else:
+        hookenv.log('installing ntpmon upstart configuration')
+        host.rsync('src/ntpmon-telegraf.upstart', upstart_config)
+    set_state('ntpmon.installed')
+    remove_state('ntpmon.configured')
+
+
+@when('ntpmon.installed')
+@when_not('ntpmon.configured')
+def configure_ntpmon():
+    """
+    Reconfigure ntpmon - does nothing at present
+    """
+    hookenv.log('configuring ntpmon')
+    set_state('ntpmon.configured')
+    remove_state('ntpmon.started')
+
+
+@when('ntpmon.configured', 'telegraf.configured')
+@when_not('ntpmon.started')
+def start_ntpmon():
+    """
+    Start the ntpmon daemon process.
+    If ntp is not installed, do nothing.
+    """
+    hookenv.log('starting ntpmon')
+    if os.path.exists(ntp_conf):
+        if host.service_running(service_name):
+            host.service_restart(service_name)
+        else:
+            host.service_start(service_name)
+    else:
+        hookenv.log('ntp not configured; not starting ntpmon')
+    set_state('ntpmon.started')
+
+
+@when_file_changed([ntp_conf], hash_type='sha256')
+def ntp_conf_updated():
+    remove_state('ntpmon.started')
+
+
+if __name__ == '__main__':
+    main()
