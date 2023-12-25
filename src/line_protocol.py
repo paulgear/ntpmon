b7105bd22b302eb15f222f5929ffc30d68fe01dd
Paul Gear <github@libertysys.com.au>
2023-12-25 18:41:04 +1000

    Automatically detect tags & numeric fields for line protocol display

--- src/line_protocol.py
+++ src/line_protocol.py
@@ -9,79 +9,27 @@
 # Ref: https://docs.influxdata.com/influxdb/v2/reference/syntax/line-protocol/
 # With telegraf we can only use timestamps in nanosecond format
 
-import socket
 
-
-integer_fields = [
-    'auth-fail',
-    'bad-header',
-    'bogus',
-    'duplicate',
-    'exceeded-max-delay',
-    'exceeded-max-delay-dev-ratio',
-    'exceeded-max-delay-ratio',
-    'invalid',
-    'interleaved',
-    'leap',
-    'local-poll',
-    'num-combined',
-    'remote-poll',
-    'stratum',
-    'sync-loop',
-    'synchronized',
-]
-
-float_fields = [
-    'delay',
-    'dispersion',
-    'freq',
-    'max-error',
-    'offset',
-    'remaining-correction',
-    'root-delay',
-    'root-dispersion',
-    'score',
-    'skew',
-    'stdev',
-]
-
-tag_fields = [
-    'mode',
-    'refid',
-    'rx-timestamp',
-    'source',
-    'tx-timestamp',
-    'type',
+exclude_fields = [
 ]
 
 exclude_tags = [
 ]
 
 
-def refstr(s: str) -> str:
-    """Convert from hex string to a printable string (if all characters are printable), or an IP
-    address string otherwise.  See https://stackoverflow.com/a/2198052/1621180 for inspiration."""
-    packed = bytes.fromhex(s)
-    all_printable = [x >= 20 and x <= 126 for x in packed]
-    if all(all_printable):
-        return ''.join([chr(x) for x in packed])
-    else:
-        return socket.inet_ntoa(packed)
-
-
-def format_tags(metric: dict, additional_tags: dict) -> str:
+def format_tags(metrics: dict, additional_tags: dict) -> str:
     return ','.join([
-        f'{transform_identifier(tag)}={metric[tag]}' for tag in tag_fields if tag in metric
+        f'{transform_identifier(tag)}={metrics[tag]}' for tag in sorted(metrics.keys()) if tag not in exclude_tags and type(metrics[tag]) == str
     ] + [
-        f'{transform_identifier(tag)}={additional_tags[tag]}' for tag in additional_tags if tag not in exclude_tags
+        f'{transform_identifier(tag)}={additional_tags[tag]}' for tag in sorted(additional_tags.keys()) if tag not in exclude_tags
     ])
 
 
 def format_fields(metrics: dict) -> str:
     return ','.join([
-        f'{transform_identifier(field)}={metrics[field]}' for field in float_fields if field in metrics
+        f'{transform_identifier(field)}={metrics[field]}' for field in sorted(metrics.keys()) if field not in exclude_fields and type(metrics[field]) == float
     ] + [
-        f'{transform_identifier(field)}={metrics[field]}i' for field in integer_fields if field in metrics
+        f'{transform_identifier(field)}={metrics[field]}i' for field in sorted(metrics.keys()) if field not in exclude_fields and type(metrics[field]) == int
     ])
 
 
